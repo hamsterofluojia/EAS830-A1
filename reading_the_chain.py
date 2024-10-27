7d027212ef14db6ef76dc3919cad9e0589470752
hamsterofluojia <61070947+hamsterofluojia@users.noreply.github.com>
2024-10-27 15:14:24 -0400

    Update reading_the_chain.py

--- reading_the_chain.py
+++ reading_the_chain.py
@@ -83,11 +83,9 @@ def is_ordered_block(w3, block_num):
 	# then block is ordered
 	ordered = all(priority_fees[i] >= priority_fees[i + 1] for i in range(len(priority_fees) - 1))
 
-	"""
 	# if it is an empty block or block with just 1 transaction, ordered is True
 	if len(priority_fees) == 0 || len(priority_fees) == 1:
 		ordered = True
-  	"""
 
 	return ordered
 
