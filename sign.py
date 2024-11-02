f0d6a97e34ee4176e73b5780a5865b77ec6e3320
hamsterofluojia <61070947+hamsterofluojia@users.noreply.github.com>
2024-11-02 13:56:55 -0400

    Update sign.py

--- sign.py
+++ sign.py
@@ -7,7 +7,7 @@ def sign(m):
     w3 = Web3()
     # create an eth account and recover the address (derived from the public key) and private key
     # your code here
-    account = Account.create()
+    account = w3.eth.account.create()
 
     eth_address = account.address  # Eth account
     private_key = account.private_key
