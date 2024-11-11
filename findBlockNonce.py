f7f919675c07be802c980ae736af0854feb72075
hamsterofluojia <61070947+hamsterofluojia@users.noreply.github.com>
2024-11-11 15:50:24 -0500

    Update findBlockNonce.py

--- findBlockNonce.py
+++ findBlockNonce.py
@@ -21,7 +21,6 @@ def mine_block(k, prev_hash, rand_lines):
     # TODO your code to find a nonce here
     nonce = 0
 
-    rand_lines_bytes = ''.join(rand_lines).encode()
     rand_lines_str = ''.join(rand_lines)
 
     prev_hash_hex = prev_hash.hex()
